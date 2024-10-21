5bf22f16f779d2649fc207dac1393c7199c969ca
RuthJimenz309 <jimenezruth81@gmail.com>
2024-10-20 22:55:41 -0600

    Merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-<<<<<<< HEAD
-import Landing from './components/Landing';
+import Landing from './components/LandingPage';
 // Añadimos nuestras rutas en el App.js
 import {ServicioPage }from './components/servicios/pages/ServicioPage';
 import {DetallesPage} from './components/servicios/pages/DetallesPage';
@@ -18,26 +17,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path="/landing" element={<Landing />} />
-=======
-import LandingPage from './components/LandingPage';
-import Home from './components/Home';
-import Equipo from './components/Equipo';
-import Contactanos from './components/Contactanos';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/equipo" element={<Equipo />} />
-        <Route path="/contactanos" element={<Contactanos />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
->>>>>>> a47dfd9af6b10cb8cf9a72ca3c9b6b415a0336f7
 
         {/*   Que me muestros servicio  que tiene la veterinaria */}
       <Route path='/servicio' element={<ServicioPage/>}></Route>
