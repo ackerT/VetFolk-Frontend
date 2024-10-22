1fe7604801490e12bc894c287538d84cf06e4894
RuthJimenz309 <jimenezruth81@gmail.com>
2024-10-22 13:33:55 -0600

    tres cambios en el formulario

--- src/components/servicios/pages/FormularioPage.jsx
+++ src/components/servicios/pages/FormularioPage.jsx
@@ -18,25 +18,33 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogTitle
+  DialogTitle,
+  ThemeProvider,
+  createTheme,
 } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
+// Horas disponibles de 8:00 a.m. a 5:00 p.m.
+const availableHours = [
+  "08:00 a.m.", "09:00 a.m.", "10:00 a.m.", "11:00 a.m.",
+  "12:00 p.m.", "01:00 p.m.", "02:00 p.m.", "03:00 p.m.",
+  "04:00 p.m.", "05:00 p.m."
+];
 
+// Estilo personalizado para los botones
 const theme = createTheme({
   typography: {
     fontFamily: 'Poppins, sans-serif',
     h5: {
       color: '#00897b', // Cambiar el color del título "Agendar Cita"
       fontSize: '1.6rem', // Ajustar tamaño de letra del título
-      fontWeight: 'bold',// En negrita el h5
+      fontWeight: 'bold', // En negrita el h5
     },
     button: {
       fontSize: '1rem', // Aumentar tamaño de letra para botones
+      fontFamily: 'Poppins, sans-serif',
     },
   },
   palette: {
@@ -60,10 +68,10 @@ const theme = createTheme({
         },
       },
     },
-
     MuiDialog: {
       styleOverrides: {
         paper: {
+          fontFamily: 'Poppins, sans-serif',
           backgroundColor: '#e8f0fe', // Color de fondo de la modal
           borderRadius: '8px', // Esquinas redondeadas
           padding: '20px', // Espaciado interno
@@ -81,24 +89,14 @@ export default function Component() {
   const [showNotification, setShowNotification] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [fecha, setFecha] = useState(dayjs());
-  const [textValue, setTextValue] = useState(fecha.format('YYYY-MM-DD'));
   const [existingAppointments, setExistingAppointments] = useState([]);
   const navigate = useNavigate();
 
   const cambiarFecha = (nuevaFecha) => {
     setFecha(nuevaFecha);
-    setTextValue(nuevaFecha.format('YYYY-MM-DD'));
   };
 
-  const handleTextChange = (event) => {
-    const nuevaFecha = dayjs(event.target.value);
-    setTextValue(event.target.value);
-    if (nuevaFecha.isValid()) {
-      setFecha(nuevaFecha);
-    }
-  };
-
-  const onSubmit = async (formData) => {
+  const onSubmit = (formData) => {
     setIsSubmitting(true);
     const hora = formData.Hora;
     const nuevaFechaFormateada = fecha.format('YYYY-MM-DD');
@@ -127,18 +125,15 @@ export default function Component() {
     setOpenModal(false);
   };
 
-  // Función para verificar si la fecha está ocupada
   const isDateOccupied = (date) => {
     const formattedDate = date.format('YYYY-MM-DD');
     return existingAppointments.some(appointment => appointment.fecha === formattedDate);
   };
 
-  return ( 
-     
+  return (
     <ThemeProvider theme={theme}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        {/* editar  el tamaño máximo del contenedor del formulario */}
-        <Container component="main" maxWidth="sm" sx={{ width: '90%', maxWidth: '400px' }}>
+        <Container component="main" maxWidth="sm">
           <Box 
             sx={{ 
               marginTop: 10, 
@@ -147,24 +142,18 @@ export default function Component() {
               alignItems: "center", 
               padding: '20px',
               width: '100%',
-              maxWidth: '600px' // Limitar el ancho máximo del formulario
+              maxWidth: '600px' 
             }}
-            >
-
-              {/* editar imagen */}
-              
-               {/* editar el tamaña del h5 */}
+          >
             <Typography component="h1" variant="h5">
               Agendar una Cita
             </Typography>
-              {/* Muestra la notificacion */}
             <Collapse in={showNotification}>
-              <Alert severity="success" sx={{ mt: 2, width: '100%', fontSize: '1.5rem' }}>
+              <Alert severity="success" sx={{ mt: 2, width: '100%' }}>
                 ¡Cita agendada con éxito!
               </Alert>
             </Collapse>
-                   {/*  formulario */}
-            <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 3 }}>
+            <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
                   <Controller
@@ -179,12 +168,8 @@ export default function Component() {
                         fullWidth
                         id="fullName"
                         label="Nombre Completo"
-                        autoFocus
                         error={Boolean(errors["Nombre Completo"])}
                         helperText={errors["Nombre Completo"]?.message}
-                        InputProps={{
-                          style: { fontSize: '1.2rem' } // Cambiar tamaño de letra del formulario
-                        }}
                       />
                     )}
                   />
@@ -204,9 +189,6 @@ export default function Component() {
                         label="Teléfono"
                         error={Boolean(errors.Telefono)}
                         helperText={errors.Telefono?.message}
-                        InputProps={{
-                          style: { fontSize: '1.3rem' } // Cambiar tamaño de letra del formulario
-                        }}
                       />
                     )}
                   />
@@ -226,144 +208,107 @@ export default function Component() {
                         label="Nombre de su Mascota"
                         error={Boolean(errors["Nombre de la Mascota"])}
                         helperText={errors["Nombre de la Mascota"]?.message}
-                        InputProps={{
-                          style: { fontSize: '1.2rem' } // Cambiar tamaño de letra del formulario
-                        }}
                       />
                     )}
                   />
                 </Grid>
                 <Grid item xs={12}>
                   <FormControl fullWidth>
-                    <InputLabel id="breed-label">Especie</InputLabel>
+                    <InputLabel id="service-type-label">Tipo de Servicio</InputLabel>
                     <Controller
-                      name="Especie"
+                      name="Tipo de Servicio"
                       control={control}
                       defaultValue=""
                       rules={{ required: "Este campo es requerido" }}
                       render={({ field }) => (
                         <Select
                           {...field}
-                          labelId="breed-label"
-                          id="breed"
-                          label="Especie"
-                          error={Boolean(errors.Especie)}
-                          InputProps={{
-                            style: { fontSize: '1.2rem' } // Cambiar tamaño de letra del formulario
-                          }}
+                          labelId="service-type-label"
+                          id="service-type"
+                          label="Tipo de Servicio"
+                          error={Boolean(errors["Tipo de Servicio"])}
                         >
-                          <MenuItem value="Perro">Perro</MenuItem>
-                          <MenuItem value="Gato">Gato</MenuItem>
-                          <MenuItem value="Caballo">Caballo</MenuItem>
-                          <MenuItem value="Otro">Otro</MenuItem>
+                          <MenuItem value="Consulta">Consulta</MenuItem>
+                          <MenuItem value="Peluqueria">Cirugía</MenuItem>
                         </Select>
                       )}
                     />
                   </FormControl>
                 </Grid>
-                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+                {/* Campo para la Hora */}
+                <Grid item xs={12}>
                   <Controller
                     name="Hora"
                     control={control}
                     defaultValue=""
                     rules={{ required: "Este campo es requerido" }}
                     render={({ field }) => (
-                      <TextField
-                        {...field}
-                        required
-                        fullWidth
-                        id="time"
-                        label="Hora"
-                        type="time"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                        inputProps={{
-                          step: 300, // 5 min
-                        }}
-                        error={Boolean(errors.Hora)}
-                        helperText={errors.Hora?.message}
-                        InputProps={{
-                          style: { fontSize: '1.2rem', textAlign: 'center' } // Cambiar tamaño de letra del input de hora
-                        }}
-                      />
+                      <FormControl fullWidth>
+                        <InputLabel id="hour-label">Hora</InputLabel>
+                        <Select
+                          {...field}
+                          labelId="hour-label"
+                          id="hour"
+                          label="Hora"
+                          error={Boolean(errors.Hora)}
+                        >
+                          {availableHours.map((hour) => (
+                            <MenuItem key={hour} value={hour}>
+                              {hour}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
                     )}
                   />
                 </Grid>
-                <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-                  <Controller
-                    name="Fecha"
-                    control={control}
-                    defaultValue={textValue}
-                    render={({ field }) => (
-                      <DateCalendar
-                        {...field}
-                        value={fecha}
-                        onChange={(newValue) => cambiarFecha(newValue)}
-                        renderDay={(day, _value, _utils) => {
-                          const isOccupied = isDateOccupied(day);
-                          return (
-                            <span style={{ color: isOccupied ? 'red' : 'inherit' }}>
-                              {day.date()}
-                            </span>
-                          );
-                        }}
-                        sx={{
-                          '& .MuiCalendarPicker-day.Mui-selected': {
-                            backgroundColor: '#00897b', // Color de fondo de la fecha seleccionada
-                          },
-                          '& .MuiCalendarPicker-day:hover': {
-                            backgroundColor: '#00897b', // Color de fondo al hacer hover en la fecha
-                          },
-                          '& .MuiTypography-root': {
-                            fontSize: '1.3rem', // Cambiar tamaño de letra del calendario
-                          },
-                          width: '80%', // Hacer el calendario más ancho
-                          maxWidth: '400px', // Limitar el ancho máximo del calendario
-                          margin: '0 auto', // Centrar el calendario
-                        }}
-                      />
-                    )}
+                <Grid item xs={12}>
+                  <Typography variant="body1">Seleccione la Fecha</Typography>
+                  <DateCalendar 
+                    value={fecha} 
+                    onChange={cambiarFecha}
+                    shouldDisableDate={isDateOccupied} 
+                    sx={{
+                      '& .MuiPaper-root': {
+                        '&:hover': {
+                          backgroundColor: '#00897b',
+                        },
+                      },
+                    }}
                   />
-                </Grid>
-                 {/* iditar botones del  formulario */}
-                <Grid item xs={12}>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    disabled={isSubmitting}
-                  >
-                    Agendar Cita
-                  </Button>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    fullWidth
-                    sx={{ backgroundColor: '#00897b', '&:hover': { backgroundColor: '#00796b' } }} // Cambiar color a #00897b
-                    onClick={handleGoBack}
-                    startIcon={<ArrowBackIcon />}
-                  >
-                    Atrás
-                  </Button>
                 </Grid>
               </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? "Agendando..." : "Agendar Cita"}
+              </Button>
+              <Button
+                onClick={handleGoBack}
+                fullWidth
+                variant="outlined"
+                sx={{ mb: 2 }}
+              >
+                Volver
+              </Button>
             </Box>
           </Box>
-           {/* Editar el modalel  */}
-          <Dialog open={openModal} onClose={handleCloseModal}>
-            <DialogTitle sx={{ fontSize: '1.5rem', color: '#00897b' }}>¡Hola!</DialogTitle>
+          <Dialog
+            open={openModal}
+            onClose={handleCloseModal}
+          >
+            <DialogTitle>Hola!</DialogTitle>
             <DialogContent>
-              <Typography sx={{ color: '#000', fontSize: '1.5rem' }}>
-                Ya existe una cita para esta fecha y hora.
+              <Typography>
+                Ya existe una cita programada para esta fecha. Por favor, elija una fecha diferente.
               </Typography>
             </DialogContent>
             <DialogActions>
-              <Button onClick={handleCloseModal} variant="contained" sx={{ color: '#fff' }}>
-                Cerrar
-              </Button>
+              <Button onClick={handleCloseModal}>Cerrar</Button>
             </DialogActions>
           </Dialog>
         </Container>
