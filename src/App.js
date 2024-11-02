d554b63cb86dc59f25a7197cee4d1babbb24e779
Ana Cantarero <isacantarero29@gmail.com>
2024-11-01 21:46:05 -0600

    Página de inicio de administración y formulario para registrar mascotas

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import Products from './components/Products';
 import AboutUs from './components/AboutUs';
 import FormularioPage from './components/FormularioPage'
 import Servicio from './components/Servicio'
+import AdminDashboard from "./components/AdminDashboard"; 
+import RegisterPet from './components/RegisterPet';
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
-    <Route path='/' element={<LandingPage/>}></Route>
+   <Route path="/admin" element={<AdminDashboard />} />
+   <Route path="/mascotas" element={<RegisterPet />} />
+   <Route path='/' element={<LandingPage/>}></Route>
     <Route path='/login' element={<Login/>}></Route>
     <Route path="/home" element={<Home />} />
     <Route path="/products" element={<Products />} />
     <Route path="/about-us" element={<AboutUs />} />
     <Route path="/agendar" element={<FormularioPage />} />
     <Route path="/servicio" element={<Servicio />} />
-
    </Routes>
    </BrowserRouter> 
   );
---
+++ src/components/AdminDashboard.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import { FaUsers, FaPaw, FaCalendarAlt, FaBoxOpen, FaStethoscope, FaFileMedical, FaUserCircle, FaSignOutAlt } from "react-icons/fa";
+import "./AdminDashboard.css";
+import logo from "../img/vet.png";
+import adminImage from "../img/admin-image.jpg";
+
+function AdminDashboard() {
+  useEffect(() => {
+    const mainMenuLi = document
+      .getElementById("mainMenu")
+      .querySelectorAll("li");
+
+    function changeActive() {
+      mainMenuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    mainMenuLi.forEach((n) => n.addEventListener("click", changeActive));
+
+    return () => {
+      mainMenuLi.forEach((n) => n.removeEventListener("click", changeActive));
+    };
+  }, []);
+
+  return (
+    <div className="admin-dashboard">
+<div className="sidebar">
+  <a href="/" className="logo-link">
+    <img src={logo} alt="VetFolk Logo" />
+  </a>
+
+  <ul id="mainMenu">
+    <li>
+      <a href="/clientes"><FaUsers /> Clientes y Personal</a>
+    </li>
+    <li>
+    <a href="/mascotas"><FaPaw /> Mascotas</a>
+    </li>
+    <li>
+      <a href="/agenda"><FaCalendarAlt /> Agenda</a>
+    </li>
+    <li>
+      <a href="/products"><FaBoxOpen /> Productos</a>
+    </li>
+    <li>
+      <a href="/services"><FaStethoscope /> Servicios</a>
+    </li>
+    <li>
+      <a href="/expedientes"><FaFileMedical /> Expedientes</a>
+    </li>
+  </ul>
+
+  <ul className="lastMenu">
+    <li>
+      <a href="#"><FaUserCircle /> Mi Cuenta</a>
+    </li>
+    <li>
+      <a href="#"><FaSignOutAlt /> Salir</a>
+    </li>
+  </ul>
+</div>
+
+      <div className="content">
+        <div className="welcome-section">
+          <div className="text-section">
+            <h1>¡Bienvenido!</h1>
+            <p>Elige un módulo y empieza a administrar VetFolk.</p>
+          </div>
+          <div className="image-section">
+            <img src={adminImage} alt="Administración VetFolk" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminDashboard;
